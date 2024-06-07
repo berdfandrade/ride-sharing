8f1be1329e744a20fc3b7087f190953f3ed81c4b
Bernardo Andrade <berdfandrade@gmail.com>
2024-06-07 18:10:44 -0300

    --

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask, jsonify 
-from calculate_distance import calculate_distance
 from pymongo import MongoClient
-from bson.objectid import ObjectId
-import math
 
+client = MongoClient('mongodb://localhost:27017/')
+db = client['riding-shiring']
+riders_collection = db['riders']
+drivers_collection = db['drivers']
 
-app = Flask(__name__)
+def create_indexes():
+    riders_collection.create_index('riderId', unique=True) 
+    drivers_collection.create_index('driverId', unique=True)
 
-@app.route('/', methods=['GET'])
-def hello():
-	return jsonify({'message' : 'Hello'} )
+# Função de teste para verificar a conexão e a criação de índices
+def test_connection():
+    try:
+        # Testar a conexão com o MongoDB
+        client.admin.command('ping')
 
+        # Testar se os índices foram criados
+        if 'riderId_1' not in riders_collection.index_information():
+            print("Índice 'riderId' não foi criado em 'riders' collection.")
+        if 'driverId_1' not in drivers_collection.index_information():
+            print("Índice 'driverId' não foi criado em 'drivers' collection.")
 
-if __name__ == '__main__': 
-	app.run(debug=True)
+        print("Conexão com o MongoDB e criação de índices bem-sucedidas.")
+    except Exception as e:
+        print(f"Erro ao testar a conexão com o MongoDB: {e}")
 
-
+if __name__ == "__main__":
+    test_connection()
